4522dfc4b6f053a6c421f20aaf8c34f55357178a
VavilenTatarskiy <levintsev@gmail.com>
2018-01-09 23:19:27 +0300

    chery pick from fix-issue843

---
+++ src/gui/gatewayer.go
@@ -0,0 +1,23 @@
+package gui
+
+import (
+	"github.com/skycoin/skycoin/src/cipher"
+	"github.com/skycoin/skycoin/src/coin"
+	"github.com/skycoin/skycoin/src/visor"
+	"github.com/skycoin/skycoin/src/visor/historydb"
+	"github.com/skycoin/skycoin/src/wallet"
+)
+
+// Gatewayer interface for Gateway methods
+type Gatewayer interface {
+	Spend(wltID string, coins uint64, dest cipher.Address) (*coin.Transaction, error)
+	GetWalletBalance(wltID string) (wallet.BalancePair, error)
+	GetWallet(wltID string) (wallet.Wallet, error)
+	UpdateWalletLabel(wltID, label string) error
+	GetAddressTxns(a cipher.Address) (*visor.TransactionResults, error)
+	GetUxOutByID(id cipher.SHA256) (*historydb.UxOut, error)
+	GetWalletUnconfirmedTxns(wltID string) ([]visor.UnconfirmedTxn, error)
+	CreateWallet(wltName string, options wallet.Options) (wallet.Wallet, error)
+	ScanAheadWalletAddresses(wltName string, scanN uint64) (wallet.Wallet, error)
+	NewAddresses(wltID string, n uint64) ([]cipher.Address, error)
+}
